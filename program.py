67741a9a087004f2437e9cf8637abf510bc417a4
James Williams <jamesphwilliams@gmail.com>
2018-12-10 19:08:05 +0000

    Implement execute function in Program class, for executing a given program

--- program.py
+++ program.py
@@ -1,15 +1,24 @@
 from typing import List
-from instruction import Instruction
+from instruction import Instruction, InstrType
+from instruction_executor import InstructionExecutor
 
 class Program:
     instructions: List[Instruction]
     
     def __init__(self):
-        Instruction("abc")
+        pass
     
-    def execute(self):
-        for instruction in instructions:
-            instruction.execute()
+    def execute(self, state):
+        if len(instructions) == 0:
+            return state
+        
+        instr = instructions[0]
+        while instr.type != InstrType.HALT:
+            label, state = InstructionExecutor.execute(instr, state)
+            instr = instructions[label]
+            
+        return state
+        
             
     def add(self, instr_str):
         instructions.append(Instruction(instr_str))
