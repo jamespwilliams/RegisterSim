2ad7427b295a23c45593efb08380db56fb70f7d5
James Williams <jamesphwilliams@gmail.com>
2018-12-10 19:02:38 +0000

    Add more complicated unit test for the instruction executor

--- tests/test_instruction_executor.py
+++ tests/test_instruction_executor.py
@@ -23,6 +23,19 @@ class TestInstruction(unittest.TestCase):
         label, state = InstructionExecutor.execute(instr, [0,0,0,1])
         self.assertEqual(label, 1)
         self.assertEqual(state, [0,0,0,0])
+        
+    def test_multiple_instructions(self):
+        state = [5,2,3,1]
+        instr1: Instruction = Instruction("R3- -> L1, L5")
+        instr2: Instruction = Instruction("R2+ -> L1")
+        
+        label, state = InstructionExecutor.execute(instr1, state)
+        label, state = InstructionExecutor.execute(instr1, state)
+        label, state = InstructionExecutor.execute(instr2, state)
+        label, state = InstructionExecutor.execute(instr1, state)
+        
+        self.assertEqual(label, 5)
+        self.assertEqual(state, [5,2,4,0])
 
 if __name__ == '__main__':
     unittest.main()
